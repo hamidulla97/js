11dcc82c3a68b07802204d32226649486c9d64c9
hamidulla97 <makhsumov9797@gmail.com>
2022-09-06 23:26:42 +0500

    js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+ alert('Welcome')
+ 
+var box = prompt('ismingizni kiriting')
+
+console.log(box)
+
+var box1 = prompt('yoshingizni')
+
+console.log(box1)
+
+var box2 = prompt('yashash manzilizni kiriting')
+
+console.log(box2)
+alert('saidga haqatan kirmohchimisz')
+
+console.log("mening ismim " + ism + " mening yoshim " + yosh + " nan" + " ho " + manzil + " hush kelibsz")
+
+btn.addEventListener('click', () => {
+    let yil = prompt("Enter your birth year");
+    let sana = 2022 - yil;
+    console.log("sening " + sana + " yoshim.");
+})
+ 
+
